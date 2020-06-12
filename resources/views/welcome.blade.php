186380291610576f46908ab94aefbe7de84d082a
Jonathan Widro <jwidro@gmail.com>
2020-06-12 01:37:04 -0700

    move from public to root

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    The Irravicable Goarn!
+                    The Irravicable Goarn!???
                 </div>
 
                 <div class="links">
